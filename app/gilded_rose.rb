aacbd1787b189ab349926bd45e73010b166ca6d7
Franklin Huichi <carloshuichi@gmail.com>
2022-03-02 20:22:27 -0500

    Refactoring the code
    Co-authored-by: juperameza <juperameza@hotmail.com>
    Co-authored-by: alanRazoMen <arazo@ucol.mx>
    Co-authored-by: Alex Silva <alexs1010@gmail.com>

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -1,60 +1,58 @@
 # frozen_string_literal: true
-class Cheese
 
-  def initialize(item)
-    @item = item
-  end
-
-  def calculate
-
-  end
-
-end
+# Logic for the item degrading
 class GildedRose
-  def initialize(items, x)
+  def initialize(items)
     @items = items
-    @asd = x
-    x.calculate1
-    x.calculate2
   end
 
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        item.quality -= 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-      elsif item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-          item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
-        end
-       end
-      item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
-      if item.sell_in.negative?
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            item.quality -= 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-          else
-            item.quality -= item.quality
-          end
-        elsif item.quality < 50
-          item.quality = item.quality + 1
-        end
+      case item.name
+      when 'Aged Brie' then  aged_brie(item)
+      when 'Backstage passes to a TAFKAL80ETC concert' then backstage(item)
+      when 'Sulfuras, Hand of Ragnaros' then sulfuras(item)
+      when 'Conjured Mana Cake' then  conjured(item)
+      else                            any_item(item)
       end
+    
     end
   end
-end
 
-class Item
-  attr_accessor :name, :sell_in, :quality
+  def any_item(item)
+    item.quality -= item.sell_in <= 0 ? 2 : 1
 
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
+    item.quality = 0 if item.quality.negative?
+    item.sell_in -= 1 
   end
 
-  def to_s
-    "#{@name}, #{@sell_in}, #{@quality}"
+  def conjured(item)
+    item.quality -= 2
+    item.quality = 0 if item.quality.negative?
+    item.sell_in -= 1 
+  end
+
+  def aged_brie(item)
+    item.quality += item.sell_in <= 0 ? 2 : 1
+    item.quality = 50 if item.quality > 50
+    item.sell_in -= 1 
+  end
+
+  def sulfuras(item)
+    item.quality = 80
+  end
+
+  def backstage(item)
+    case item.sell_in
+    when 6..10
+      item.quality += 2
+    when 1..5
+      item.quality += 3
+    when item.sell_in..0
+      item.quality = 0
+    else
+      item.quality += 1
+    end
+    item.sell_in -= 1 
   end
 end
--- tests/gilded_rose_spec.rb
+++ tests/gilded_rose_spec.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require File.join("#{File.dirname(__FILE__)}/../app", 'gilded_rose')
+require File.join("#{File.dirname(__FILE__)}/../app", 'item')
+
 
 describe GildedRose do
   before(:each) do
-    @items = [
+    @items =  [
       Item.new('+5 Dexterity Vest', 10, 20),
       Item.new('Aged Brie', 2, 0),
       Item.new('Elixir of the Mongoose', 5, 7),
@@ -14,79 +16,88 @@ describe GildedRose do
       Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 40),
       Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 40),
       Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 40),
-      # This Conjured item does not work properly yet
-      Item.new('Conjured Mana Cake', 3, 6), # <-- :O
+      Item.new('Conjured Mana Cake', 3, 6),
       Item.new('Aged Brie', 0, 0)
     ]
+    @rose = GlidedRose.new(@items)
   end
 
   describe '#update_quality' do
     it 'does not change the name' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[0].name).to eq '+5 Dexterity Vest'
     end
 
     it 'Changes the quality of the aged brie ' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[1].quality).to eq 1
     end
     it 'Changes correctly the quality of aged brie when the sellin date has passed' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items.last.quality).to eq 2
     end
 
     it 'Changes the quality of any item by 1 ' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[2].quality).to eq 6
     end
 
     it 'Sulfuras never change quality' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[3].quality).to eq 80
     end
 
     it 'Sulfuras never change sell in date' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[3].sell_in).to eq 0
     end
 
     it 'The quality is never negative' do
-      50.times { GildedRose.new(@items).update_quality }
+      cycle(@rose)
       expect(@items[0].quality).to be > -1
     end
 
     it 'Once the sell by date has passed, Quality degrades twice as fast' do
-      11.times { GildedRose.new(@items).update_quality }
+      cycle(@rose, 11)
       expect(@items[0].quality).to eq 8
     end
 
     it "Quality of an item shouldn't be greater than 50" do
-      60.times { GildedRose.new(@items).update_quality }
+     cycle(@rose, 60)
       expect(@items.last.quality).to be <= 50
     end
 
     it 'Backstage passes increase quality' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[5].quality).to eq 21
     end
     it 'Backstage passes increase quality twice as fast when sellin date is less than 10' do
-      GildedRose.new(@items).update_quality
+     @rose
 
       expect(@items[6].quality).to eq 42
     end
     it 'Backstage passes increase quality thrice as fast when sellin date is less than 5' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[7].quality).to eq 43
     end
 
     it 'Backstage passes quality drops to 0 after sellin date' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[8].quality).to eq 0
     end
 
     it 'Conjured items degrade in quality twice as fast' do
-      GildedRose.new(@items).update_quality
+     @rose
       expect(@items[9].quality).to eq 4
     end
+    it 'Conjured items shouldnt have negative quality' do
+      cycle(@rose)
+      expect(@items[9].quality).to be > -1
+    end
   end
+end
+
+
+def cycle (rose,many=50)
+  many.times {rose }
 end
